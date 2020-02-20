e3203d112d8bf4704d6c41e6177f0ffb5b4f2216
Prashaanth Kannan <pkannan@libertyglobal.com>
2020-02-20 14:19:18 +0100

    Initial commit

---
+++ src/test/java/com/example/helloworld/HelloWorldApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class HelloWorldApplicationTests {
+
+	void contextLoads() {
+	}
+
+}
